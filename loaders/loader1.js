add6a25e40fd42e523e291064c129d36a5ed5c1e
ViavaCos <13811092694@163.com>
2021-09-07 19:59:54 +0800

    配置一个啥也不处理的loader

---
+++ loaders/loader1.js
@@ -0,0 +1,11 @@
+/**
+ * loader本质上是一个函数
+ *   content 内容
+ *   map 映射关系
+ *   meta 元数据
+ */
+module.exports = function(content, map, meta) {
+    console.log(content);
+
+    return content
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,21 @@
-module.exports = {
+const path = require('path')
 
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                // loader: path.resolve(__dirname, 'loaders', 'loader1')
+                // 通过resolveLoader配置查找loader路径后就可以将上面的简写成这样
+                loader: 'loader1'
+            }
+        ]
+    },
+    resolveLoader: {
+        modules: [
+            path.resolve(__dirname, 'loaders'),
+            'node_modules'
+        ]
+    },
+    mode: 'development'
 }
