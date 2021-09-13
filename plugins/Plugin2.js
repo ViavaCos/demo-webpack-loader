3a3652c8dbc4d0fc9cc70c4567eff9c6a20e1d0c
ViavaCos <13811092694@163.com>
2021-09-13 20:59:55 +0800

    向输出的资源中添加一个a.js

--- plugins/Plugin2.js
+++ plugins/Plugin2.js
@@ -1,8 +1,26 @@
 class Plugin2 {
     apply(complier){
-        complier.hooks.thisCompilation.tap('Plugin2', complation => {
-            debugger
-            console.log(complation);
+        complier.hooks.thisCompilation.tap('Plugin2', compilation => {
+            // debugger
+            // console.log(compilation);
+
+            const content = 'var a = 2;\nconsole.log(a);'
+            compilation.hooks.additionalAssets.tapAsync('Plugin2', (cb) => {
+                // 往输出的资源中添加一个a.js
+                compilation.assets['a.js'] = {
+                    // 文件大小
+                    size(){
+                        return content.length
+                    },
+
+                    // 文件内容
+                    source(){
+                        return content
+                    }
+                }
+
+                cb()
+            })
         })
     }
 }
