d6d3a2efd52341217bc003f5fa3411e1c313f57c
ViavaCos <13811092694@163.com>
2021-09-13 20:39:39 +0800

    plugin的同步触发方式

--- plugins/Plugin1.js
+++ plugins/Plugin1.js
@@ -4,6 +4,25 @@ class Plugin1 {
         complier.hooks.emit.tap('Plugin123', (compalation) => {
             console.log('emit-tap', 10086);
         })
+
+        // 同步 tapAsync 会额外接收一个参数cb, 当cb调用后，声明周期才会继续往下走
+        complier.hooks.emit.tapAsync('Plugin123', (compalation, cb) => {
+            
+            setTimeout(() => {
+                console.log('emit-tapAsync', 10086);
+                cb()
+            }, 1000)
+        })
+
+        // 同步 Promise版本
+        complier.hooks.emit.tapPromise('Plugin123', (compalation) => {
+            return new Promise(resolve=>{
+                setTimeout(() => {
+                    console.log('tapPromise', 10086);
+                    resolve()
+                }, 2000)
+            })
+        })
     }
 }
 
